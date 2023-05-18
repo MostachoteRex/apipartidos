a97e357c9f2440e932773ab2590dd60994650281
MostachoteRex <luisaj512@gmail.com>
2023-05-17 23:45:14 -0500

    Terminacion

--- src/main/java/com/fesc/apipartidos/services/IPartidoService.java
+++
@@ -1,7 +0,0 @@
-package com.fesc.apipartidos.services;
-
-import com.fesc.apipartidos.shared.PartidoDto;
-
-public interface IPartidoService {
-    public PartidoDto crearPartido(PartidoDto partidoCrearDto);
-}
